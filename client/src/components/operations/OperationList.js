db054fe4bda092eef72a98959d38f7e3b913f2a3
pzakirov <zakirov@cmsplanet.ru>
2019-05-22 11:00:16 +0300

    datepicker initial state

--- client/src/components/operations/OperationList.js
+++ client/src/components/operations/OperationList.js
@@ -7,8 +7,19 @@ import { getAllOperations } from '../../actions/expenseActions';
 
 class OperationList extends Component {
 
-    componentDidMount(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: new Date()
+        }
+    }
+
+    componentDidMount() {
         this.props.getAllOperations();
+    }
+
+    handeDateChange = e => {
+        this.setState({ startDate: e })
     }
 
     render() {
@@ -17,17 +28,23 @@ class OperationList extends Component {
             <div className="col s12 m6">
                 <div className="section">
                     <div>
-                        <span onClick={setOpened.bind(this, Modal.CreateOperation, true)}
-                            className="btn-floating right btn-large waves-effect waves-light red">
-                            <i className="material-icons">add</i>
-                        </span>
                         <h5>Операции</h5>
                     </div>
-                    <div>
-                        <ul className="collection">
-                            {operations && operations.map((x, i) => <OperationCard key={`operation-${i}`} operation={x} />)}
-                        </ul>
-                    </div>
+                    <span>Тут должен был быть datepicker</span>
+                    {
+                        !!operations.length && (
+                            <div>
+                                <ul className="collection">
+                                    {operations.map((x, i) => <OperationCard key={`operation-${i}`} operation={x} />)}
+                                </ul>
+                            </div>
+                        )
+                    }
+                    {!operations.length && <span>Нет операций за выбранный период</span>}
+                    <span onClick={setOpened.bind(this, Modal.CreateOperation, true)}
+                        className="btn-floating btn-medium waves-effect waves-light red">
+                        <i className="material-icons">add</i>
+                    </span>
                 </div>
             </div>
         );
